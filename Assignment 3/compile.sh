a94b74cc7022f3e01b6eeccc254edba0f6a8e6fb
Mr Squid <47063311+MrSquid25@users.noreply.github.com>
2019-01-28 20:23:03 +0100

    Update compile.sh

--- Assignment 3/compile.sh
+++ Assignment 3/compile.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+//Filename: Compile.sh
+//Author: MrSquid
+//Purpose: Assembly any nasm code. In this case, Egg_Hunter.nasm
 
 echo '[+] Assembling with Nasm ... '
-nasm -f elf32 -o $1.o $1.nasm
+nasm -f elf32 -o Egg_Hunter.o Egg_Hunter.nasm
 
 echo '[+] Linking ...'
-ld -o $1 $1.o
+ld -o Egg_Hunter Egg_Hunter.o
 
 echo '[+] Done!'
 
