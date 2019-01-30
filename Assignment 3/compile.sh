92c35b434872200eb564c2357b0648a49e6ca8aa
Mr Squid <47063311+MrSquid25@users.noreply.github.com>
2019-01-30 19:01:29 +0100

    Update compile.sh

--- Assignment 3/compile.sh
+++ Assignment 3/compile.sh
@@ -12,7 +12,10 @@ ld -o Egg_Hunter Egg_Hunter.o
 echo '[+] Done!'
 
 objdump=$(objdump -d ./Egg_Hunter|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g')
+#Dump the shellcode
 
-cp skeleton_shellcode.c shellcode.c 
+cp skeleton_shellcode.c shellcode.c #Copy the skeleton shellcode 
 
 replace "shellcode" "$objdump" -- shellcode.c &>/dev/null #Replace shellcode dump
+
+gcc -fno-stack-protector -z execstack shellcode.c -o shellcode &>/dev/null #Compile and execute the shellcode
