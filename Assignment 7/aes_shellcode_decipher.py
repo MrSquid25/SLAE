9209766b397fbfa94be4540763e26ce74772906c
Mr Squid <47063311+MrSquid25@users.noreply.github.com>
2019-02-02 16:51:25 +0100

    Update aes_shellcode_decipher.py

--- Assignment 7/aes_shellcode_decipher.py
+++ Assignment 7/aes_shellcode_decipher.py
@@ -23,8 +23,7 @@ def decipher_message(key, ciphertext, iv): #Decipher function
 def main():
 	iv= "166fe2294df5d0f3" #Initialization vector
 	key= "N2FlMjE4ZmYyOTI4ZjZiMg==" #Base64 Key, it must be 24 due to is AES-192
-	cipher_shell = "\xca\xb5\x85\xf2\xdd\xa7\x85\x19\x8a\xfe\x1b\x7d\xbb\x02\xd7\x7a\x3c\x1e\x74\x8e\x1e\xe3\x22\x5c\x8f\x22\x36\x26\x30\xa1\x6c\xc1\x8b\xcd\x1a\x60\xd8\xd6\xe7\x10\xf1\x8b\x0c\x38\x77\x85\x25\x74\xe0\x04\xbe\xe4\x7a\x57\x0d\x09\xc6\xa5\xda\x41\xf6\x9e\xa8\xc0\x8b\x69\x9f\x38\xa3\x17\xec\x52\x21\x3b\x9c\x74\x5a\xf4\x8a\x4c"
-	#Cipher shell is the shellcode ciphered by AES	
+	cipher_shell = "string_to_be_replaced" #Here, main.sh will inject the encrypted shellcode to be deciphered	
 	shellcode = ""	
 	for x in bytearray(cipher_shell): #Cipher shell converted to string (\x is erased)
 		shellcode += '%02x' % x
