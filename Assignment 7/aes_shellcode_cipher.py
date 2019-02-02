006d156ec0846c951194f4ff6117fb8f245becd1
Mr Squid <47063311+MrSquid25@users.noreply.github.com>
2019-02-02 16:50:36 +0100

    Update aes_shellcode_cipher.py

--- Assignment 7/aes_shellcode_cipher.py
+++ Assignment 7/aes_shellcode_cipher.py
@@ -24,8 +24,7 @@ def cipher_message(key, message, iv): #Cipher function
 def main():
 	iv= "166fe2294df5d0f3"
 	key= "N2FlMjE4ZmYyOTI4ZjZiMg==" #Key length must be 24 due to AES-192
-	shell_to_cipher = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80"
-	#Execve-nasm shellcode
+	shell_to_cipher = "string_to_be_replaced" #Here, main.sh will inject the shellcode to cipher
 	shellcode = ""	
 	for x in bytearray(shell_to_cipher): #Shellcode format is set to string --> \x32 is set as x32 for example 
 		shellcode += 'x'
