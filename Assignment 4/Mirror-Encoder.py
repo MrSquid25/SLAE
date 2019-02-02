fb88c077385b36aaaaa4ea5c3157568b24fd94f3
Mr Squid <47063311+MrSquid25@users.noreply.github.com>
2019-02-02 11:37:30 +0100

    Update Mirror-Encoder.py

--- Assignment 4/Mirror-Encoder.py
+++ Assignment 4/Mirror-Encoder.py
@@ -18,9 +18,9 @@ for x in bytearray(shellcode):  #Add to shell the shellcode as hexadecimal forma
 shell2 = ""
 for i in range(0,4*len(bytearray(shellcode)),8): #Loop which takes 2 bytes and swap its order
 	if (len(shell[i+1:i+4]) + len(shell[i+5:i+8]))==6:
-		shell3 += "0"+ shell[i+5:i+8] + "," + "0"+shell[i+1:i+4] + ","
+		shell2 += "0"+ shell[i+5:i+8] + "," + "0"+shell[i+1:i+4] + ","
 	elif (len(shell[i+1:i+4]) + len(shell[i+5:i+8]))==3: #If the length of the shellcode is not even, it is added 0xf0 to make it even. 
-		shell3 += "0xf0" + "," + "0"+shell[i+1:i+4] + ","
+		shell2 += "0xf0" + "," + "0"+shell[i+1:i+4] + ","
 
 
 print shell2 + "0xf0,0xf0" #Marker to finish the decoder
