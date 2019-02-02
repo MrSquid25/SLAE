769376ec65a8da1caeab327942289567614417d5
Mr Squid <47063311+MrSquid25@users.noreply.github.com>
2019-02-02 17:44:10 +0100

    Update compile_bind_shell.sh

--- Assignment 1/compile_bind_shell.sh
+++ Assignment 1/compile_bind_shell.sh
@@ -30,5 +30,18 @@ else
 		echo "[+] Shellcode with port $2 is..."
 		echo 
 		objdump -d ./$1|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+		echo $objdump 
+		sleep 3
+
+		echo "[+] Compiling shellcode.c"
+
+		cp skeleton_shellcode.c shellcode.c  #Copy the skeleton of the shellcode (it contais a string named shellcode which it will be replaced with objdump)
+
+		replace "shellcode" "$objdump" -- shellcode.c &>/dev/null #Replace shellcode dump
+
+		sleep 2
+
+		gcc -fno-stack-protector -z execstack shellcode.c -o shellcode &>/dev/null #Compile and execute the shellcode
+	
 	fi
 fi
