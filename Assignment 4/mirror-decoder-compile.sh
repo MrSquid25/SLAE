2e0ab29214e1e4942ff4a290cd0574972ea66898
Mr Squid <47063311+MrSquid25@users.noreply.github.com>
2019-02-07 18:23:57 +0100

    Update mirror-decoder-compile.sh

--- Assignment 4/mirror-decoder-compile.sh
+++ Assignment 4/mirror-decoder-compile.sh
@@ -5,18 +5,18 @@
 
 
 echo '[+] Assembling with Nasm ... '
-nasm -f elf32 -o mirror-decode.o mirror-decode.nasm
+nasm -f elf32 -o mirror-decoder.o mirror-decoder.nasm
 
 echo '[+] Linking ...'
-ld -o mirror-decode mirror-decode.o
+ld -o mirror-decoder mirror-decoder.o
 
 echo '[+] Done!'
 
 sleep 2
 
-echo "[+] Dumping disassembling code.."
+echo "[+] Dumping disassembly code.."
 
-objdump=$(objdump -d ./mirror-decode|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g')
+objdump=$(objdump -d ./mirror-decoder|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g')
 
 echo $objdump 
 sleep 3
